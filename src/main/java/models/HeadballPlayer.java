32c7c955883b0bf3a248c6a6ea96283c473f4e1c
gunestuzer <elifgunestuzer@gmail.com>
2026-04-06 18:34:27 +0300

    Added FootballPlayer and HeadballPlayer classes. Edited BaseTeam and BasePlayer classes.

---
+++ src/main/java/models/HeadballPlayer.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.util.Random;
+
+public class HeadballPlayer extends BasePlayer{
+
+    private int heading;
+    private static final Random r = new Random();
+    public HeadballPlayer(String name, int age, double fitness){
+        super(name, age, fitness);
+        this.heading = 50;
+    }
+
+    @Override
+    public void train(){
+        if(isInjured){
+            System.out.println(name + " is injured and cannot train!");
+            return;
+        }
+        fitness = Math.min(100, fitness + 10);
+        fitness = Math.max(0, fitness - 2);
+        heading += 7;
+        if(r.nextInt(100) < 10){
+            isInjured = true;
+            System.out.println(name + " got injured!");
+        }
+    }
+    @Override
+    public void recover(){
+        fitness = Math.min(100, fitness + 15);
+        if(isInjured && fitness > 60){
+            isInjured = false;
+            System.out.println(name + " got recovered!");
+        }
+    }
+    @Override
+    public int getSkillLevel(){
+        int skill = (int) ((heading + fitness) / 2);
+        if(isInjured){
+            skill = (int) (skill * 0.5);
+        }
+        return skill;
+    }
+}
